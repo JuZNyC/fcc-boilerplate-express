e79439e50c6c57f34723cd30071fe0ea3c00c03e
JuZNyC <61894460+JuZNyC@users.noreply.github.com>
2025-04-25 16:47:03 -0400

    completed fcc-chain-middleware-to-create-a-time-server

--- myApp.js
+++ myApp.js
@@ -1,16 +1,41 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
+// app.get('/', (req, res) =>{
+//     res.send('Hello Express');
+// });
 
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/views/index.html');
+// });
 
+// app.use('/public', express.static(__dirname + '/public'));
 
+// console.log('Hello World');
 
+// app.get('/json', (req, res) => {
+//     if (process.env.MESSAGE_STYLE === 'uppercase') {
+//         res.json({ message: 'Hello json'.toUpperCase() });
+//     }
+//     else {
+//         res.json({ message: 'Hello json' });
+//     }
+// });
 
+// app.use('/', (req, res, next) => {
+//     console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+//     next();
+// });
 
-
-
-
-
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({
+        "time" : req.time
+    })
+});
 
 
 
