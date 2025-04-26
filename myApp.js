904d4c76b68f16dbc37a557c8819ffd94d086558
JuZNyC <61894460+JuZNyC@users.noreply.github.com>
2025-04-26 07:57:24 -0400

    completed fcc basic node and express

--- myApp.js
+++ myApp.js
@@ -1,41 +1,43 @@
 let express = require('express');
 let app = express();
 require('dotenv').config();
+let bodyParser = require("body-parser");
+
+
+app.use('/', (req, res, next) => {
+    console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+    next();
+});
 
 // app.get('/', (req, res) =>{
 //     res.send('Hello Express');
 // });
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/views/index.html');
-// });
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
 
-// app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
-// console.log('Hello World');
+console.log('Hello World');
 
-// app.get('/json', (req, res) => {
-//     if (process.env.MESSAGE_STYLE === 'uppercase') {
-//         res.json({ message: 'Hello json'.toUpperCase() });
-//     }
-//     else {
-//         res.json({ message: 'Hello json' });
-//     }
-// });
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({ message: 'Hello json'.toUpperCase() });
+    }
+    else {
+        res.json({ message: 'Hello json' });
+    }
+});
 
-// app.use('/', (req, res, next) => {
-//     console.log(req.method + ' ' + req.path + ' - ' + req.ip);
-//     next();
-// });
-
-// app.get('/now', (req, res, next) => {
-//     req.time = new Date().toString();
-//     next();
-// }, (req, res) => {
-//     res.json({
-//         "time" : req.time
-//     })
-// });
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({
+        "time" : req.time
+    })
+});
 
 app.get("/:word/echo", (req, res) => {
     let word = req.params.word;
@@ -44,26 +46,27 @@ app.get("/:word/echo", (req, res) => {
     })
 });
 
+app.get("/name", (req, res) => {
+    let name = req.query
+    res.json({
+        name: `${name.first} ${name.last}`
+    });
+});
 
+// app.use(bodyParser.urlencoded({
+//     extended : false
+// }));
 
+app.post("/name", bodyParser.urlencoded({extended : false}) ,(req, res) => {
+    res.json({
+        name: req.body.first + " " + req.body.last
+    });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (require.main === module) {
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+}
 
  module.exports = app;
